a8433144933ab9a88d28e9a7306b710c82f803d6
ahmspd <ahmspdi@gmail.com>
2022-03-23 14:48:31 +0700

    update baseservice

--- LinovHrCommunity/src/main/java/com/lawencon/linovhrcommunity/service/BaseServiceLinovCommunityImpl.java
+++ LinovHrCommunity/src/main/java/com/lawencon/linovhrcommunity/service/BaseServiceLinovCommunityImpl.java
@@ -2,6 +2,15 @@ package com.lawencon.linovhrcommunity.service;
 
 import com.lawencon.base.BaseServiceImpl;
 
-public class BaseServiceLinovCommunityImpl extends BaseServiceImpl {
+public class BaseServiceLinovCommunityImpl extends BaseServiceImpl  {
+	public String generateCode(int input) {
+        String codeString = "AEL0987654321";
+        StringBuilder sb = new StringBuilder(input);
 
+        for (int i = 0; i < input; i++) {
+            int index = (int) (codeString.length() * Math.random());
+            sb.append(codeString.charAt(index));
+        }
+        return sb.toString();
+    }
 }
